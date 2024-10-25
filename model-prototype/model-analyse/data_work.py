2b127699799af763d108f60dacc5902da976f49c
eyestrain77 <72514643+eyestrain77@users.noreply.github.com>
2024-10-25 23:01:46 +0300

    Add files via upload
    data_work_funk - функции для анализа, выделения категорий и тд
    data_work - код который вызывает эти функции)

---
+++ model-prototype/model-analyse/data_work.py
@@ -0,0 +1,28 @@
+from data_work_func import *
+from sentence_transformers import SentenceTransformer
+
+# Загружаем модель для создания embedding-векторов
+model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+
+# считаем данные с файла
+with open('md_data/data.md') as f:
+    text = []
+    full_text = []
+    # удалим пунктуацию и пустые строки
+    for i in f:
+        full_text.append(i)
+        i=i.replace('\n', '')
+        i=remove_punctuation(i)
+        if len(i.strip()) != 0:
+            text.append(i)
+
+
+categories = [i.split('\t')[0] for i in text[6:93]]
+# используем функции из файла data_work_func
+real_categories = split_into_categories(categories)
+# пользователь вводит вопрос и происходит поиск лучшей категории
+question = input()
+best_option = find_best_cos_sim(question, real_categories)
+# получение текста документации для категории
+find_context(best_option.lower(), full_text)
+
---
+++ model-prototype/model-analyse/data_work_func.py
@@ -0,0 +1,62 @@
+import string
+from sentence_transformers import util
+import numpy as np
+
+# уберает всю пунктуацию с помощью библиотеки string 
+def remove_punctuation(text):
+    translator = str.maketrans('', '', string.punctuation) 
+    return text.translate(translator)
+
+# Загрузите оглавление, получите разбиение по категориям, подходящее для дальнейшего использования
+def split_into_categories(categories):
+    cat = []
+    place = '1'
+    work_path = ''
+    for i in categories:
+        # если у категории есть продолжение, прыгаем глубже
+        if place+'1' == i.split()[0]:
+            work_path += '. '+' '.join(i.split()[1:]) # обновляем название категории
+            place += '1' # обновляем номер категории
+        # достигли финальной точки категории, но это не самая первая категория (у первой категории всегда есть подкатегория, поэтому рассматривается отдельно)
+        elif len(i.split()[0])>1:
+            cat.append(work_path[2:]) # сохраняем полное название категории
+            work_path = '. '.join(work_path.split('. ')[:len(place)])+'. '+' '.join(i.split()[1:]) # обновляем название категории
+            place = i.split()[0] # обновляем номер категории
+        else:
+            cat.append(work_path[2:]) # сохраняем полное название категории
+            work_path = '. '+' '.join(i.split()[1:]) # обновляем название категории
+            place = i.split()[0] # обновляем номер категории
+    return cat
+
+# поиск описания категории в документации. На вход подается категория и полная документация
+def find_context(title, data):
+    ind = 0
+    # обнаружение индекса категории
+    for i in range(len(data)):
+        if ('<a name="_toc' in data[i].lower()) and (title in data[i].lower()):
+            ind = i+1
+    context = ''
+    # поиск описания категории
+    for i in data[ind:]:
+        if '<a name="_toc' in i:
+            # если началась другая категория заканчиваем описание
+            break
+        context += i+'\n'
+    return context
+
+
+def cosine_similarity(text1, text2, model):
+    # Преобразуем текст в embedding-векторы
+    embedding1 = model.encode(text1, convert_to_tensor=True)
+    embedding2 = model.encode(text2, convert_to_tensor=True)
+    
+    # Вычисляем косинусное сходство
+    cosine_sim = util.pytorch_cos_sim(embedding1, embedding2)
+    
+    return cosine_sim.item()  # Возвращаем значение сходства как float
+
+def find_best_cos_sim(question, text):
+    cos_sim = []
+    for i in text:
+        cos_sim.append(cosine_similarity(i, question))
+    return text[cos_sim.index(max(cos_sim))]

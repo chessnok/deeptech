6dd81e3811ac25bca5db59cdf9810f2a342c03ac
Mikhail Fadin <fadin.edu@gmail.com>
2024-10-26 17:33:19 +0300

    TG full 3

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -53,6 +53,9 @@ def get_conversation_id(user_id: int):
     stmt = select(User.c.conversation_id).where(User.c.tg_id == user_id)
     return result[0] if (result := conn.execute(stmt).fetchone()) else None
 
+def get_user_id(message_id: int):
+    stmt = select(Message_author_map.c.author_id).where(Message_author_map.c.message_id == message_id)
+    return result[0] if (result := conn.execute(stmt).fetchone()) else None
 
 @bot.message_handler(commands=['start'], func=lambda message: message.chat.type == 'private')
 def send_welcome(message):
@@ -68,10 +71,6 @@ def change_conversation_id(message):
     new_conversation(message.from_user.id)
     bot.reply_to(message, "Надеюсь я ответил на твой предыдущий вопрос) Очищаю контекст переписки. Просто напиши "
                           "свой новый вопрос о документации")
-
-
-def send_question_to_admin(message):
-    bot.send_message(admin_group_id, message)
 
 
 @bot.message_handler(func=lambda message: message.chat.type == 'private')
@@ -92,9 +91,6 @@ def process_message(message: Message):
         markup = InlineKeyboardMarkup()
         button = InlineKeyboardButton("Отправить вопрос администраторам", callback_data="send_to_admins")
         markup.add(button)
-        conn.execute(
-            Message_author_map.insert().values(message_id=message.message_id, author_id=message.from_user.id)
-        )
         conn.commit()
         bot.reply_to(message, "Мы не смогли найти ответ на ваш вопрос. "
                               "Однако вы можете обратиться за помощью к администратору.", reply_markup=markup)
@@ -103,31 +99,28 @@ def process_message(message: Message):
 @bot.callback_query_handler(func=lambda call: call.data == "send_to_admins")
 def handle_callback(call: CallbackQuery):
     user = call.from_user
-    question_text = f"Вопрос от {user.first_name} {user.last_name or ''} (@{user.username or 'нет юзернейма'}):"
-    bot.send_message(admin_group_id, question_text)
-    if call.message.reply_to_message:
-        original_message = call.message.reply_to_message
-        bot.forward_message(admin_group_id, original_message.chat.id, original_message.message_id)
+    question_text = f"Вопрос от {user.first_name} {user.last_name or ''} (@{user.username or 'нет юзернейма'}): {call.message.reply_to_message.text}"
+    msg = bot.send_message(admin_group_id, question_text)
+    if msg:
+        conn.execute(
+            Message_author_map.insert().values(message_id=msg.message_id,
+                                               author_id=user.id)
+        )
         bot.answer_callback_query(call.id, "Ваш вопрос отправлен администраторам.")
         bot.send_message(call.message.chat.id, "Ваш вопрос был отправлен администраторам. Ожидайте ответа.")
     else:
         bot.send_message(call.message.chat.id, "Ошибка при отправке вопроса.")
 
 
-@bot.message_handler(func=lambda message: message.chat.type == 'group')
+@bot.message_handler(func=lambda message: message.chat.id == int(admin_group_id))
 def check_reply_message(message: Message) -> None:
     """
     Проверяет, является ли сообщение ответом, и если да, отправляет текст ответа нужному пользователю.
     """
     if message.reply_to_message:
-        original_message_id = message.reply_to_message.id
-        if (
-            author_record := Message_author_map.select()
-            .where(Message_author_map.c.message_id == original_message_id)
-            .first()
-        ):
-            author_id = author_record.user_id  # замените на корректное название поля с ID пользователя1
-            # Отправляем сообщение с текстом ответа
+        original_message_id = message.reply_to_message
+        author_id = get_user_id(original_message_id.message_id)
+        if author_id:
             bot.send_message(
                 chat_id=author_id,
                 text=f"Ответ администраторов:\n{message.text}"
